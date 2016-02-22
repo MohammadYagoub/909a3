0a31fb012bf7dc7f35bec1ccc84e732fce421f77
MohammadYagoub <MohammadA.Yagoub@gmail.com>
2016-02-22 10:35:03 -0700

    updated the getHistory method

--- cse360assign3/src/cse360assign3/Calculator.java
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -1,13 +1,21 @@
 package cse360assign3;
 
+/** this class creates a calculator that adds, subtracts,
+ * multiplies, divides, and keeps a history of operations. 
+ * @author Mohammad Yagoub
+ * Pin: 909
+ * Version 02-22-2015
+ */
+
 public class Calculator {
 
 	private int total;
-	
+	private String history;
 	/** creates a constructor of Calculator
 	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
 	/** returns total
@@ -17,28 +25,34 @@ public class Calculator {
 		return total;
 	}
 	
-	/** adds value to total
+	/** adds value to total and updates history
 	 * @param value to be added
 	 */
 	public void add (int value) {
 		total = total + value;
+		
+		history = history + " + " + value;
 	}
 	
-	/** subtracts value from total
+	/** subtracts value from total and updates history
 	 * @param value to be subtracted
 	 */
 	public void subtract (int value) {
 		total = total - value;
+		
+		history = history + " - " + value;
 	}
 	
-	/** multiplies total by value
+	/** multiplies total by value and updates history
 	 * @param value to be multiplied by
 	 */
 	public void multiply (int value) { 
 		total = total * value;
+		
+		history = history + " * " + value;
 	}
 	
-	/** divides total by value
+	/** divides total by value and updates history
 	 * @param value to be divided by
 	 */
 	public void divide (int value) { 
@@ -46,13 +60,15 @@ public class Calculator {
 			total = 0;
 		else
 			total = total / value;
+		
+		history = history + " / " + value;
 	}
 	
-	/** returns empty string
-	 * @return empty string
+	/** returns a string that shows all the operations done
+	 * @return history of operation
 	 * 
 	 */
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
