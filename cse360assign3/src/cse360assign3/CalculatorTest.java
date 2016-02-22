9d83e37843c13defd8c4248d6618d5198a8e957f
MohammadYagoub <MohammadA.Yagoub@gmail.com>
2016-02-22 10:21:33 -0700

    All tests have been implemented and updated.

--- cse360assign3/src/cse360assign3/CalculatorTest.java
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -15,32 +15,52 @@ public class CalculatorTest {
 
 	
 	@Test
-	public void getTotal() {
-		fail("Not yet implemented"); //fail
+	public void testGetTotal() {
+		Calculator calc = new Calculator();
+		assertEquals(0, calc.getTotal()); //test if get total is returning the correct value
 	}
 	
 	@Test 
-	public void add() {
-		fail("Not yet implemented"); //fail
+	public void testAdd() {
+		Calculator calc = new Calculator();
+		calc.add(3); //add 3 to total
+		assertEquals(3, calc.getTotal()); //test if total is updated correctly
 	}
 	
 	@Test
-	public void subtract() {
-		fail("Not yet implemented"); //fail
+	public void testSubtract() {
+		Calculator calc = new Calculator();
+		calc.subtract(3); //subtract 3 from total
+		assertEquals(-3, calc.getTotal()); //test if total is updated correctly
 	}
 	
 	@Test
-	public void multiply () {
-		fail("Not yet implemented"); //fail
+	public void testMultiply () {
+		Calculator calc = new Calculator();
+		calc.add(3); //add 3 to total
+		calc.multiply(3); //multiply total by 3
+		assertEquals(9, calc.getTotal()); //test if total is updated correctly
 	}
 	
 	@Test
-	public void divide () {
-		fail("Not yet implemented"); //fail
+	public void testDivideByNonZero () {
+		Calculator calc = new Calculator();
+		calc.subtract(3); //subtract 3 from total
+		calc.divide(3); //divide total by 3
+		assertEquals(-1, calc.getTotal()); //test if total is updated correctly
 	}
 	
 	@Test
-	public void getHistory () {
-		fail("Not yet implemented"); //fail
+	public void testDivideByZero () {
+		Calculator calc = new Calculator();
+		calc.subtract(3); //subtract 3 from total
+		calc.divide(0); //divide total by 0
+		assertEquals(0, calc.getTotal()); //test if total is updated correctly
+	}
+	
+	@Test
+	public void testGetHistory () {
+		Calculator calc = new Calculator();
+		assertEquals("", calc.getHistory());
 	}
 }
