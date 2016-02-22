38898bf2fc92425d0767cdc965826cd3b068d9d8
MohammadYagoub <MohammadA.Yagoub@gmail.com>
2016-02-22 10:04:18 -0700

    JUnit test class CalculatorTest added. Javadoc documentation added to class Calculator.

--- cse360assign3/src/cse360assign3/Calculator.java
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -4,30 +4,51 @@ public class Calculator {
 
 	private int total;
 	
+	/** creates a constructor of Calculator
+	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/** returns total
+	 * @return total
+	 */
 	public int getTotal () {
 		return 0;
 	}
 	
+	/** adds value to total
+	 * @param value to be added
+	 */
 	public void add (int value) {
 		
 	}
 	
+	/** subtracts value from total
+	 * @param value to be subtracted
+	 */
 	public void subtract (int value) {
 		
 	}
 	
-	public void multiply (int value) {
+	/** multiplies total by value
+	 * @param value to be multiplied by
+	 */
+	public void multiply (int value) { 
 		
 	}
 	
-	public void divide (int value) {
+	/** divides total by value
+	 * @param value to be divided by
+	 */
+	public void divide (int value) { 
 		
 	}
 	
+	/** returns empty string
+	 * @return empty string
+	 * 
+	 */
 	public String getHistory () {
 		return "";
 	}
---
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,46 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		Calculator calc = new Calculator();
+		
+		assertNotNull(calc); //test if object is not null
+	}
+
+	
+	@Test
+	public void getTotal() {
+		fail("Not yet implemented"); //fail
+	}
+	
+	@Test 
+	public void add() {
+		fail("Not yet implemented"); //fail
+	}
+	
+	@Test
+	public void subtract() {
+		fail("Not yet implemented"); //fail
+	}
+	
+	@Test
+	public void multiply () {
+		fail("Not yet implemented"); //fail
+	}
+	
+	@Test
+	public void divide () {
+		fail("Not yet implemented"); //fail
+	}
+	
+	@Test
+	public void getHistory () {
+		fail("Not yet implemented"); //fail
+	}
+}
